7d1c1342f4d055cf01e6ea93a7bc667393ab25c5
zahir248 <muhdzahir248@gmail.com>
2024-02-18 13:47:58 +0800

    add /ePortfolio/ subdirectory to href title portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
     return (
         <nav className={styles.navbar}>
-            <a className={styles.title} href="/">
+            <a className={styles.title} href="/ePortfolio/">
                 Portfolio
             </a>
             <div className={styles.menu}>
